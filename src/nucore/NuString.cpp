b3c1ce875826663b24d30a5e9862cd7589587e2a
cr7jrsuii <195530488+cr7jrsuii@users.noreply.github.com>
2026-04-04 23:16:38 +0100

    Add `NuStrChr` (#14)
    Co-authored-by: Nintendocustom <nintendocustomyt@gmail.com>

--- src/nucore/NuString.cpp
+++ src/nucore/NuString.cpp
@@ -1,3 +1,5 @@
+#include "nucore/NuString.h"
+
 int NuStrCpy(char* dst, const char* src) {
     char* d = dst;
     if (src && *src) {
@@ -10,4 +12,13 @@ int NuStrCpy(char* dst, const char* src) {
     }
     *d = 0;
     return (int)(d - dst);
+}
+
+char* NuStrChr(char* str, char c) {
+    while (*str != '\0') {
+        if (*str == c)
+            return str;
+        str++;
+    }
+    return 0;
 }
