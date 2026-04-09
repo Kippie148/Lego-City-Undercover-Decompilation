f112f8465b522ced054ec370d82443cea810d3df
Nintendocustom <nintendocustomyt@gmail.com>
2026-04-09 20:19:29 +0200

    Fix formatting of `SAction_UI_SetStatsGoldBricks`

--- src/kestrel/script/actions/SAction_UI_SetStatsGoldBricks.cpp
+++ src/kestrel/script/actions/SAction_UI_SetStatsGoldBricks.cpp
@@ -4,11 +4,9 @@ const char* SAction_UI_SetStatsGoldBricks::GetName() const {
     return "UI_SetStatsGoldBricks";
 }
 void SAction_UI_SetStatsGoldBricks::GetInputs(SCmdParams& params) const {
-
-        params.SanityCheck();
-        params.AddParam(SV_NUMBER);
-        params.AddParam(SV_NUMBER);
-
+    params.SanityCheck();
+    params.AddParam(SV_NUMBER);
+    params.AddParam(SV_NUMBER);
 }
 void SAction_UI_SetStatsGoldBricks::GetOutputs(SCmdParams& params) const {
     params.SanityCheck();
