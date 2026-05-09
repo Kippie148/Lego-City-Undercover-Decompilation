33c9376394e0c493aed5a08ae3514c312abe47d4
Nintendocustom <nintendocustomyt@gmail.com>
2026-05-09 20:47:00 +0200

    Refactor memory allocation functions in chroma_mem.cpp

--- src/chroma/chroma_mem.cpp
+++ src/chroma/chroma_mem.cpp
@@ -1,26 +1,18 @@
-#include "chroma_mem.h"
+#include "chroma/chroma_mem.h"
 #include "nn/types.h"
 
-void malloc (ulong size);
-void free(void *__ptr);
+void malloc(size_t __size);
+void free(void* __ptr);
 
 void Chroma_Alloc(unsigned int size) {
-    return malloc((ulong)size);
+    return malloc(size);
 }
-
 void Chroma_AllocAligned(unsigned int size, unsigned int alignment) {
-    return malloc((ulong)size);
+    return malloc(size);
 }
-
 void Chroma_Free(void* ptr) {
-    if(ptr != nullptr) {
-        free(ptr);
-    }
+    free(ptr);
 }
-
 void Chroma_FreeAligned(void* ptr) {
-    if(ptr != nullptr) {
-        free(ptr);
-    }
+    free(ptr);
 }
-
