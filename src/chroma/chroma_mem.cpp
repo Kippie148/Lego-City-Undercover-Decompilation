73dfce7cfdd96a595a295499d967f456b9a41851
Kippie148 <kruipjip@gmail.com>
2026-05-09 21:13:30 +0200

    Chroma mem functions (#15)
    * Add files via upload
    
    * Refactor memory allocation functions in chroma_mem.cpp
    
    * Update csv
    
    ---------
    
    Co-authored-by: Nintendocustom <nintendocustomyt@gmail.com>

---
+++ src/chroma/chroma_mem.cpp
@@ -0,0 +1,18 @@
+#include "chroma/chroma_mem.h"
+#include "nn/types.h"
+
+void malloc(size_t __size);
+void free(void* __ptr);
+
+void Chroma_Alloc(unsigned int size) {
+    return malloc(size);
+}
+void Chroma_AllocAligned(unsigned int size, unsigned int alignment) {
+    return malloc(size);
+}
+void Chroma_Free(void* ptr) {
+    free(ptr);
+}
+void Chroma_FreeAligned(void* ptr) {
+    free(ptr);
+}
